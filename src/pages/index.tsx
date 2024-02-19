cb8884783e7ae2ca707c13f9c370ae22f67fda54
Gyulo94 <qkrcjfrb112@gmail.com>
2024-02-19 17:33:17 +0900

    글작성 인증 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import PostList from '../components/PostList';
-
 export default function Home() {
   return (
     <main>
       <h1>NEXTJS HELLO WORLD</h1>
-      <PostList />
     </main>
   );
 }
