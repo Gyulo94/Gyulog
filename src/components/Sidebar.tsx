eb852643bd0f5f74ef2a741213b2fcd926a125f2
Gyulo94 <qkrcjfrb112@gmail.com>
2024-02-19 20:34:52 +0900

    사이드바 추가기능 구현

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,9 @@
 import { cn } from '@/src/utils/style';
+import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
 import { FC } from 'react';
 import { AiFillGithub, AiFillInstagram, AiOutlineClose } from 'react-icons/ai';
+import { createClient } from '../utils/supabase/client';
 import { IconButton } from './IconButton';
 
 type SidebarProps = {
@@ -9,7 +11,17 @@ type SidebarProps = {
   isOpen: boolean;
 };
 
+const supabase = createClient();
+
 const Sidebar: FC<SidebarProps> = ({ close, isOpen }) => {
+  const { data: existingCategories } = useQuery({
+    queryKey: ['categories'],
+    queryFn: async () => {
+      const { data } = await supabase.from('Post').select('category');
+      return Array.from(new Set(data?.map((d) => d.category)));
+    },
+  });
+
   return (
     <div
       className={cn(
@@ -24,17 +36,20 @@ const Sidebar: FC<SidebarProps> = ({ close, isOpen }) => {
         홈
       </Link>
       <Link
-        href="/tag"
+        href="/tags"
         className="w-48 font-medium text-gray-600 hover:underline"
       >
         태그
       </Link>
-      <Link
-        href="/category/web-Development"
-        className="w-48 font-medium text-gray-600 hover:underline"
-      >
-        Web Development
-      </Link>
+      {existingCategories?.map((category) => (
+        <Link
+          href={`/category/${category}`}
+          className="w-48 font-medium text-gray-600 hover:underline"
+          key={category}
+        >
+          {category}
+        </Link>
+      ))}
       <div className="mt-10 flex items-center gap-4">
         <IconButton
           Icon={AiFillInstagram}
@@ -48,6 +63,7 @@ const Sidebar: FC<SidebarProps> = ({ close, isOpen }) => {
           href="https://www.github.com/gyulo94"
           target="_blank"
         />
+        <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgyulog.vercel.app&count_bg=%23555555&title_bg=%23555555&icon=bilibili.svg&icon_color=%23E7E7E7&title=%EB%B0%A9%EB%AC%B8%EC%9E%90&edge_flat=false" />
       </div>
     </div>
   );
---
+++ src/pages/tags/index.tsx
@@ -0,0 +1,31 @@
+import { createClient } from '@/src/utils/supabase/client';
+import { useQuery } from '@tanstack/react-query';
+import Link from 'next/link';
+
+const supabase = createClient();
+
+export default function Tag() {
+  const { data: existingTags } = useQuery({
+    queryKey: ['tags'],
+    queryFn: async () => {
+      const { data } = await supabase.from('Post').select('tags');
+      return Array.from(new Set(data?.flatMap((d) => JSON.parse(d.tags))));
+    },
+  });
+  return (
+    <div className="flex flex-col items-center gap-2 px-4 pb-24 pt-20">
+      <h1 className="mb-8 text-center text-2xl font-semibold">태그</h1>
+      <div className="container mx-auto flex flex-wrap justify-center gap-2 px-10">
+        {existingTags?.map((tag) => (
+          <Link
+            href={`/tags/${tag}`}
+            key={tag}
+            className="text-xl underline text-slate-500 hover:text-gray-700"
+          >
+            {tag}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
