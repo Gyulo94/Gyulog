2ced2affad1b44575d03da39d732a0e7235e1dca
Gyulo94 <qkrcjfrb112@gmail.com>
2024-04-20 10:21:58 +0900

    Footer 추가

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.Footer}>
+      <p>@Copyright by 찰규 </p>
+    </footer>
+  );
+};
+
+export default Footer;
