7ba576985b4a3654ab2099e5d3866defd9198e42
Gyulo94 <qkrcjfrb112@gmail.com>
2024-04-20 13:28:06 +0900

    라우팅

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <Header />
-        <footer />
-      </div>
-    </>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <footer />
+    </div>
   );
 }
 
