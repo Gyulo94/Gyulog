80094c138cebee7891b72cf2e85a188014828e3e
gyulo94 <qkrcjfrb112@gmail.com>
2024-07-25 16:26:33 +0900

    commit

---
+++ server/src/main/java/com/server/gyulog/GyulogApplication.java
@@ -0,0 +1,13 @@
+package com.server.gyulog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyulogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyulogApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/server/gyulog/GyulogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.gyulog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyulogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
